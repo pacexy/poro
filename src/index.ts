ad24c41d2932c4fc696381100672f8eb8b15c05c
pacexy <pacexy@gmail.com>
2021-08-18 21:37:33 +0800

    fix(cdragon): export raw cdragon domain

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,11 @@ export * from './riot/apis/dtos'
 export * from './riot/apis/types'
 export * from './riot/apis/inputs'
 
-export { cdragon, CDRAGON_DOMAIN } from './riot/static/cdragon'
+export {
+  cdragon,
+  CDRAGON_DOMAIN,
+  RAW_CDRAGON_DOMAIN,
+} from './riot/static/cdragon'
 export { ddragon } from './riot/static/ddragon'
 export { general } from './riot/static/general'
 export { axiosInstance } from './riot/static/axios'
