4247432b292bc113e9380eb8267516a93937dfaf
pacexy <pacexy@gmail.com>
2021-12-14 09:07:08 +0800

    refactor(general): replace doc literal strings with method

--- src/riot/static/general.ts
+++ src/riot/static/general.ts
@@ -1,38 +1,16 @@
 import { Platform } from '../apis/enums'
 
 import { DDRAGON_BASE_URL } from './ddragon'
-
-const GENERAL_BASE_URL = 'https://static.developer.riotgames.com'
+import { DocFile } from './general.type'
 
 /**
  * When looking up specific seasons, queues, maps and modes it is important
  * to use the correct ids.
  */
 export const general = {
-  /**
-   * Season ids are used in match history to indicate which season a match
-   * was played. A full list of season ids can be found in the file below.
-   */
-  seasons: GENERAL_BASE_URL + '/docs/lol/seasons.json',
-  /**
-   * Queue ids show up in several places throughout the API and are used to
-   * indicate which kind of match was played. A full list of queue ids can
-   * be found in the file below.
-   */
-  queues: GENERAL_BASE_URL + '/docs/lol/queues.json',
-  /**
-   * Map ids are used in match history to indicate which map a match was
-   * played. A full list of map ids can be found in the file below.
-   */
-  maps: GENERAL_BASE_URL + '/docs/lol/maps.json',
-  /**
-   * A full list of game modes can be found in the file below.
-   */
-  gameModes: GENERAL_BASE_URL + '/docs/lol/gameModes.json',
-  /**
-   * A full list of game types can be found in the file below.
-   */
-  gameTypes: GENERAL_BASE_URL + '/docs/lol/gameTypes.json',
+  doc(file: DocFile) {
+    return `https://static.developer.riotgames.com/docs/lol/${file}.json`
+  },
   /**
    * You can find all valid Data Dragon versions in the versions file.
    * Typically there's only a single build of Data Dragon for a given patch,
