6b63d316f0a7d228382cfc36aa310ee908b30286
pacexy <pacexy@gmail.com>
2025-03-21 20:37:30 +0800

    chore(static): update cdragon type

--- src/riot/static/cdragon.type.ts
+++ src/riot/static/cdragon.type.ts
@@ -1,8 +1,16 @@
-export type QueuesJSON = {
-  [queueId: string]: {
-    name: string
-    shortName: string
-    description: string
-    detailedDescription: string
-  }
-}
+const queueExamples = [
+  {
+    id: 420,
+    name: 'Ranked Solo/Duo',
+    shortName: 'Ranked Solo/Duo',
+    description: 'Ranked Solo/Duo',
+    detailedDescription: '',
+    gameSelectModeGroup: 'kSummonersRift',
+    gameSelectCategory: 'kPvP',
+    gameSelectPriority: 20,
+    isSkillTreeQueue: false,
+    hidePlayerPosition: false,
+  },
+]
+
+export type QueuesJSON = typeof queueExamples
